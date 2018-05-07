66ef7d429fab5c5cfcb13e38a729a5bf6ca9aa42
Jared Lander <info@jaredlander.com>
2018-05-07 10:20:55 -0400

    Added formula

---
+++ code/glmnet.r
@@ -0,0 +1,19 @@
+library(glmnet)
+library(useful)
+library(coefplot)
+library(magrittr)
+
+lots <- readRDS('data/manhattan_Train.rds')
+dim(lots)
+head(lots)
+View(lots)
+
+valueFormula <- TotalValue ~ FireService + 
+    ZoneDist1 + ZoneDist2 + Class + LandUse + 
+    OwnerType + LotArea + BldgArea + ComArea + 
+    ResArea + OfficeArea + RetailArea + 
+    GarageArea + FactryArea + NumBldgs + 
+    NumFloors + UnitsRes + UnitsTotal + 
+    LotFront + LotDepth + BldgFront + 
+    BldgDepth + LotType + Landmark + BuiltFAR +
+    Built + HistoricDistrict
