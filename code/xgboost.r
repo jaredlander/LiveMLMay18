e1d694209522a304b755d77f3963c9e8dbe40997
Jared Lander <info@jaredlander.com>
2018-05-08 09:38:47 -0400

    Adding xgboost file

---
+++ code/xgboost.r
@@ -0,0 +1,15 @@
+lotsTrain <- readRDS('data/manhattan_Train.rds')
+lotsTest <- readRDS('data/manhattan_Test.rds')
+lotsVal <- readRDS('data/manhattan_Validate.rds')
+
+View(lotsTrain)
+
+histFormula <- HistoricDistrict ~ FireService + 
+    ZoneDist1 + ZoneDist2 + Class + LandUse + 
+    OwnerType + LotArea + BldgArea + ComArea + 
+    ResArea + OfficeArea + RetailArea + 
+    GarageArea + FactryArea + NumBldgs + 
+    NumFloors + UnitsRes + UnitsTotal + 
+    LotFront + LotDepth + BldgFront + 
+    BldgDepth + LotType + Landmark + BuiltFAR +
+    Built + TotalValue - 1
